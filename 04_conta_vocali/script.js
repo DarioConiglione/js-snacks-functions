e83c26646f0b123747e439b1717b23de42e7379b
DarioConiglione <dariocng13@gmail.com>
2025-10-17 16:43:00 +0100

    snack 4 done

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,12 +4,24 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-function verificaVocali() {
+function verificaVocali(parola) {
 
     const vocali = ["a", "e", "i", "o", "u"];
+    let conta = 0;
+
+    for (i = 0; i < parola.length; i++) {
+        if (vocali.includes(parola[i].toLowerCase())) {
+            conta++;
+        }
+    }
+
+    return conta;
 }
 
+
 // Invoca la funzione qui e stampa il risultato in console
+verificaVocali(word);
+console.log(verificaVocali(word));
 
 
 
